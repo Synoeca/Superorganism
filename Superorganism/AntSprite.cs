4024ac4efd4fec2dc248f90862a9a346eaa32940
Synoeca <synoeca523@ksu.edu>
2024-08-30 18:54:16 -0500

    Finished project 1

--- Superorganism/AntSprite.cs
+++ Superorganism/AntSprite.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using CollisionExample.Collisions;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -19,8 +20,34 @@ namespace Superorganism
 		private Texture2D texture;
 
 		private Vector2 position = new(200, 200);
+		public Vector2 Position()
+		{
+			return position;
+		}
 
-		private bool flipped;
+		private Vector2 velocity = Vector2.Zero;
+
+		private bool _flipped;
+
+		private BoundingRectangle bounds = new BoundingRectangle(new Vector2(200 - 16, 200 - 16), 32, 32);
+
+		/// <summary>
+		/// The bounding volume of the sprite
+		/// </summary>
+		public BoundingRectangle Bounds => bounds;
+
+		private bool _isOnGround = true;
+		public bool IsOnGround()
+		{
+			return _isOnGround;
+		}
+
+		private float _gravity = 0.5f;
+		private float _groundLevel = 400;
+		private float _jumpStrength = -14f;
+		private float _movementSpeed = 3f;
+		private float _friction = 0.8f;
+
 
 		public Color Color { get; set; } = Color.White;
 
@@ -38,44 +65,68 @@ namespace Superorganism
 			gamePadState = GamePad.GetState(0);
 			keyboardState = Keyboard.GetState();
 
-			float movementSpeed = (keyboardState.IsKeyDown(Keys.LeftShift) || keyboardState.IsKeyDown(Keys.RightShift)) ? 2f : 1f;
+			_movementSpeed = (keyboardState.IsKeyDown(Keys.LeftShift) || keyboardState.IsKeyDown(Keys.RightShift)) ? 2.5f : 1f;
 
 			// Apply the gamepad movement with inverted Y axis
 			position += gamePadState.ThumbSticks.Left * new Vector2(1, -1);
-			if (gamePadState.ThumbSticks.Left.X < 0) flipped = true;
-			if (gamePadState.ThumbSticks.Left.X > 0) flipped = false;
+			if (gamePadState.ThumbSticks.Left.X < 0) _flipped = true;
+			if (gamePadState.ThumbSticks.Left.X > 0) _flipped = false;
 
-			// Apply keyboard movement
-			if (keyboardState.IsKeyDown(Keys.Up) || keyboardState.IsKeyDown(Keys.W))
-			{
-				position += new Vector2(0, -movementSpeed);
-			}
-
-			if (keyboardState.IsKeyDown(Keys.Down) || keyboardState.IsKeyDown(Keys.S))
-			{
-				position += new Vector2(0, movementSpeed);
-			}
-
+			// Handle horizontal movement input
 			if (keyboardState.IsKeyDown(Keys.Left) || keyboardState.IsKeyDown(Keys.A))
 			{
-				position += new Vector2(-movementSpeed, 0);
-				flipped = true;
+				velocity.X = -_movementSpeed;
+				_flipped = true;
 			}
-			if (keyboardState.IsKeyDown(Keys.Right) || keyboardState.IsKeyDown(Keys.D))
+			else if (keyboardState.IsKeyDown(Keys.Right) || keyboardState.IsKeyDown(Keys.D))
 			{
-				position += new Vector2(movementSpeed, 0);
-				flipped = false;
+				velocity.X = _movementSpeed;
+				_flipped = false;
+			}
+			else if (_isOnGround)
+			{
+				// Apply friction to gradually stop the sprite when on the ground
+				velocity.X *= _friction;
+				if (Math.Abs(velocity.X) < 0.1f)
+				{
+					velocity.X = 0;
+				}
+			}
+
+			// Handle vertical movement input (jump)
+			if (_isOnGround && keyboardState.IsKeyDown(Keys.Space))
+			{
+				velocity.Y = _jumpStrength;
+				_isOnGround = false; // The sprite is now in the air
+			}
+
+			// Apply gravity to the velocity
+			velocity.Y += _gravity;
+
+			// Update position based on velocity
+			position += velocity;
+
+			// Simple ground collision detection
+			if (position.Y >= _groundLevel)
+			{
+				position.Y = _groundLevel; // Keep the sprite on the ground
+				velocity.Y = 0; // Stop the downward velocity when on the ground
+				_isOnGround = true;
 			}
 
 			// Update the bounds
-			//bounds.X = position.X - 16;
-			//bounds.Y = position.Y - 16;
+			bounds.X = position.X - 16;
+			bounds.Y = position.Y - 16;
+
+			velocity.X = MathHelper.Clamp(velocity.X, -_movementSpeed * 2, _movementSpeed * 2);
 		}
 
 		public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
 		{
-			SpriteEffects spriteEffects = (flipped) ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+			SpriteEffects spriteEffects = (_flipped) ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
 			spriteBatch.Draw(texture, position, null, Color, 0, new Vector2(120, 120), 0.25f, spriteEffects, 0);
 		}
+
+		
 	}
 }
--- Superorganism/Superorganism.cs
+++ Superorganism/Superorganism.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SpriteExample;
+using System;
 
 namespace Superorganism
 {
@@ -9,7 +11,15 @@ namespace Superorganism
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
 
+		private CropSprite[] _crops;
+		private int _cropsLeft;
+		private BatSprite[] _bats;
 		private AntSprite _ant;
+		private AntEnemySprite _antEnemy;
+
+		private Texture2D _groundTexture;
+		private int _groundHeight = 100;
+		private int _groundY = 400;
 
 		public Superorganism()
 		{
@@ -20,8 +30,43 @@ namespace Superorganism
 
 		protected override void Initialize()
 		{
-			// TODO: Add your initialization logic here
+			// Initialize crops
+			Random rand = new();
+			_crops =
+			[
+				new(new Vector2((float)rand.NextDouble() * GraphicsDevice.Viewport.Width, 383)),
+				new(new Vector2((float)rand.NextDouble() * GraphicsDevice.Viewport.Width, 383)),
+				new(new Vector2((float)rand.NextDouble() * GraphicsDevice.Viewport.Width, 383)),
+				new(new Vector2((float)rand.NextDouble() * GraphicsDevice.Viewport.Width, 383)),
+				new(new Vector2((float)rand.NextDouble() * GraphicsDevice.Viewport.Width, 383)),
+				new(new Vector2((float)rand.NextDouble() * GraphicsDevice.Viewport.Width, 383)),
+				new(new Vector2((float)rand.NextDouble() * GraphicsDevice.Viewport.Width, 383)),
+				new(new Vector2((float)rand.NextDouble() * GraphicsDevice.Viewport.Width, 383)),
+				new(new Vector2((float)rand.NextDouble() * GraphicsDevice.Viewport.Width, 383)),
+				new(new Vector2((float)rand.NextDouble() * GraphicsDevice.Viewport.Width, 383)),
+				new(new Vector2((float)rand.NextDouble() * GraphicsDevice.Viewport.Width, 383)),
+				new(new Vector2((float)rand.NextDouble() * GraphicsDevice.Viewport.Width, 383)),
+			];
+			_cropsLeft = _crops.Length;
+
+			int numberOfBats = rand.Next(3, 21);
+			_bats = new BatSprite[numberOfBats];
+
+			for (int i = 0; i < numberOfBats; i++)
+			{
+				float xPos = rand.Next(0, 800);
+				float yPos = rand.Next(0, 600);
+				Direction randomDirection = (Direction)rand.Next(0, 4);
+
+				_bats[i] = new BatSprite
+				{
+					Position = new Vector2(xPos, yPos),
+					Direction = randomDirection
+				};
+			}
+
 			_ant = new AntSprite();
+			_antEnemy = new AntEnemySprite();
 
 			base.Initialize();
 		}
@@ -29,8 +74,18 @@ namespace Superorganism
 		protected override void LoadContent()
 		{
 			_spriteBatch = new SpriteBatch(GraphicsDevice);
+			foreach (CropSprite crop in _crops)
+			{
+				crop.LoadContent(Content);
+			}
+			foreach (BatSprite bat in _bats)
+			{
+				bat.LoadContent(Content);
+			}
 			_ant.LoadContent(Content);
-
+			_antEnemy.LoadContent(Content);
+			_groundTexture = new Texture2D(GraphicsDevice, 1, 1);
+			_groundTexture.SetData(new[] { new Color(139, 69, 19) });
 			// TODO: use this.Content to load your game content here
 		}
 
@@ -39,8 +94,23 @@ namespace Superorganism
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 				Exit();
 
-			// TODO: Add your update logic here
 			_ant.Update(gameTime);
+			_antEnemy.Update(gameTime, _ant.Position());
+
+			_ant.Color = Color.White;
+			foreach (CropSprite crop in _crops)
+			{
+				if (!crop.Collected && crop.Bounds.CollidesWith(_ant.Bounds))
+				{
+					_ant.Color = Color.Red;
+					crop.Collected = true;
+					_cropsLeft--;
+				}
+			}
+			foreach (BatSprite bat in _bats)
+			{
+				bat.Update(gameTime);
+			}
 
 			base.Update(gameTime);
 		}
@@ -48,10 +118,20 @@ namespace Superorganism
 		protected override void Draw(GameTime gameTime)
 		{
 			GraphicsDevice.Clear(Color.CornflowerBlue);
-
-			// TODO: Add your drawing code here
+			
 			_spriteBatch.Begin();
+			foreach (CropSprite crop in _crops)
+			{
+				crop.Draw(gameTime, _spriteBatch);
+			}
+			foreach (BatSprite bat in _bats)
+			{
+				bat.Draw(gameTime, _spriteBatch);
+			}
+			// Draw the ground texture
+			_spriteBatch.Draw(_groundTexture, new Rectangle(0, _groundY, _graphics.PreferredBackBufferWidth, _groundHeight), Color.White);
 			_ant.Draw(gameTime, _spriteBatch);
+			_antEnemy.Draw(gameTime, _spriteBatch);
 			_spriteBatch.End();
 
 			base.Draw(gameTime);
